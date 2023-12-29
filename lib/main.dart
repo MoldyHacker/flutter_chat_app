6ffc074f2f958f29dd11832e6e1df70e87cfa7bf
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-28 22:37:43 -0600

    Add Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'screens/auth.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
