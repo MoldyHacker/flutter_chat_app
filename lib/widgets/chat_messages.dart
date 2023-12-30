aef729d055c2e8c9070e4d982d38750afb862819
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-30 12:02:05 -0600

    Add chat messages and new message widgets to ChatScreen

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  // final List<ChatMessage> messages;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('No messages yet!'),
+    );
+  }
+}
