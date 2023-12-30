87aeb6e62bd84bd4fa21ce21d3a3dcc8a72d3718
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-30 13:20:08 -0600

    Add push notification setup to ChatScreen

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    final notificationSettings = await fcm.requestPermission();
+
+    final token = await fcm.getToken(); // send to backend to store for later use with user
+    print(token);
+
+    // FirebaseMessaging.onMessage.listen((message) {
+    //   print(message);
+    // });
+
+    // FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    //   print(message);
+    // });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
